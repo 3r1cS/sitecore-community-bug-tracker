635aa792e313b5cee959b1cc8e7617c08f5551bc
Eric <Eric>
2023-03-14 19:47:39 +0100

    cli test

---
+++ src/SitecoreCommunityBugTracker.Cli/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SitecoreCommunityBugTracker.Cli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(args[0]);
+        }
+    }
+}
